269455959ae35fdc72a0153a9ca5874d1e9e214c
imbni <mbnism@gmial>
2016-04-06 10:05:43 -0400

    latest-changes
    06-04-2016

--- js/digimeg.js
+++ js/digimeg.js
@@ -1,81 +1,1 @@
-
-// TODO : Transformation to the modular structure
-
-$( function () {
-
-    ///////////////////////////////////////////////
-    // Mouse over event for the Main menu items //
-    ///////////////////////////////////////////////
-    $( '.digimeg-main-nav>li>a' ).on( 'mouseover', function () {
-
-        // Get the subnav's ID from [data-subnav] attribute of the currently hovered menu item
-        var navContentId = $( this ).data( 'subnav' );
-        // Stop (in case the sub nav was sliding up) and slidedown
-        $( '#' + navContentId ).stop().slideDown( { queue: false } );
-
-        ////////////////////////////////////////////////////////////////
-        // show the little arrow on the currently hovered menu item //
-        ////////////////////////////////////////////////////////////////
-        var $el = $( this );
-        var mark = $el.find( '.digimeg-active-mark' );
-
-        mark.css( {
-            'left': ( $el.outerWidth() / 2 ) - ( mark.outerWidth() / 2 ),
-            'top': $el.outerHeight()
-        } ).show();
-        ////////////////////////////////////////////////////////////////
-
-    } );
-
-    //////////////////////////////////////////////
-    // Mouse out event for the Main menu items //
-    //////////////////////////////////////////////
-    $( '.digimeg-main-nav>li>a' ).on( 'mouseout', function () {
-
-        // Find the subnav's id from the [data-subnav] attribute of the currently hovered item
-        var navContentId = $( this ).data( 'subnav' );
-        // Stop ( in case the sub nav was sliding down ) and slide up
-        $( '#' + navContentId ).stop().slideUp( { queue: false } );
-
-        ////////////////////////////////////////////////////////////////////////////////
-        // Hide the active mark from the Menu item on which the event was triggered //
-        ////////////////////////////////////////////////////////////////////////////////
-        var $el = $( this );
-        var mark = $el.find( '.digimeg-active-mark' );
-
-        mark.css( {
-            'left': ( $el.outerWidth() / 2 ) - ( mark.outerWidth() / 2 ),
-            'top': 0
-        } ).hide();
-        //////////////////////////////////////////////////////////////////////////////////
-    } );
-
-
-    // Events to make the subnav's stay when they are being hovered over
-
-    /////////////////////////////////////////////////
-    // When the mouse is hovering over the subnav //
-    /////////////////////////////////////////////////
-    $( '.digimeg-sub-nav>li' ).on( 'mouseover', function () {
-
-        // Stop ( in case the subnav was sliding up) and slide down
-        $( this ).stop().slideDown( { queue: false } );
-
-    } );
-
-    /////////////////////////////////////////////////
-    // When the mouse has moved out of the subnav //
-    /////////////////////////////////////////////////
-    $( '.digimeg-sub-nav>li' ).on( 'mouseout', function () {
-        // Stop ( in case the sub nav was sliding down ) and slide up
-        $( this ).stop().slideUp( { queue: false } );
-    } );
-
-
-
-    $( '.digimeg-sub-nav>li .sub-anchor' ).click( function ( ) {
-        $('.digimeg-sub-nav>li').stop().slideUp( { queue: false } );
-        $.scrollTo( '#main-content', 1000, { offset: { top: -35 } } );
-    } );
-
-} );
+// TODO : Transformation to the modular structure$( function () {    ///////////////////////////////////////////////    // Mouse over event for the Main menu items //    ///////////////////////////////////////////////    $( '.digimeg-main-nav>li>a' ).on( 'mouseover', function () {        // Get the subnav's ID from [data-subnav] attribute of the currently hovered menu item        var navContentId = $( this ).data( 'subnav' );        // Stop (in case the sub nav was sliding up) and slidedown        $( '#' + navContentId ).stop().fadeToggle( 150,{ queue: false } );        ////////////////////////////////////////////////////////////////        // show the little arrow on the currently hovered menu item //        ////////////////////////////////////////////////////////////////        var $el = $( this );        var mark = $el.find( '.digimeg-active-mark' );        mark.css( {            'left': ( $el.outerWidth() / 2 ) - ( mark.outerWidth() / 2 ),            'top': $el.outerHeight()        } ).show();        ////////////////////////////////////////////////////////////////    } );    //////////////////////////////////////////////    // Mouse out event for the Main menu items //    //////////////////////////////////////////////    $( '.digimeg-main-nav>li>a' ).on( 'mouseout', function () {        // Find the subnav's id from the [data-subnav] attribute of the currently hovered item        var navContentId = $( this ).data( 'subnav' );        // Stop ( in case the sub nav was sliding down ) and slide up        $( '#' + navContentId ).stop().fadeToggle( { queue: false } );        ////////////////////////////////////////////////////////////////////////////////        // Hide the active mark from the Menu item on which the event was triggered //        ////////////////////////////////////////////////////////////////////////////////        var $el = $( this );        var mark = $el.find( '.digimeg-active-mark' );        mark.css( {            'left': ( $el.outerWidth() / 2 ) - ( mark.outerWidth() / 2 ),            'top': 0        } ).hide();        //////////////////////////////////////////////////////////////////////////////////    } );    // Events to make the subnav's stay when they are being hovered over    /////////////////////////////////////////////////    // When the mouse is hovering over the subnav //    /////////////////////////////////////////////////    $( '.digimeg-sub-nav>li' ).on( 'mouseover', function () {        // Stop ( in case the subnav was sliding up) and slide down        $( this ).stop().fadeToggle( { queue: false } );    } );    /////////////////////////////////////////////////    // When the mouse has moved out of the subnav //    /////////////////////////////////////////////////    $( '.digimeg-sub-nav>li' ).on( 'mouseout', function () {        // Stop ( in case the sub nav was sliding down ) and slide up        $( this ).stop().fadeToggle( { queue: false } );    } );    $( '.digimeg-sub-nav>li .sub-anchor' ).click( function ( ) {        $( '.digimeg-sub-nav>li' ).stop().fadeToggle( { queue: false } );        $.scrollTo( '#main-content', 1000, { offset: { top: -35 } } );    } );} );
