4df8a72d9f8c89ba7c9130e485574a6aee8c0678
imbni <mbnism@gmail.com>
2022-07-12 15:04:00 -0400

    add mlqa app

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
 const domain = process.env.MAILGUN_DOMAIN;
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+
 const fromEmail = 'Excited User';
 const toEmails = ['mbnism@gmail.com'];
 
