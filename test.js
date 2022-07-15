80808f5887519d8698c4ca8e219543a588247028
imbni <mbnism@gmail.com>
2022-07-14 23:46:50 -0400

    test attachment

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-console.log(process.env.MAILGUN_API_KEY);
-
+//console.log(process.env.MAILGUN_API_KEY);
+const fs = require('fs');
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
@@ -10,16 +10,17 @@ const fromEmail = 'Excited User <mbnism@gmail.com>';
 const toEmails = ['mbnism@gmail.com'];
 
 //const mailgunLogo = fs.createReadStream(`${__dirname}/mailgun.png`);
-//const rackspaceLogo = fs.createReadStream(`${__dirname}/rackspace.png`);
+const rackspaceLogo1 = fs.createReadStream(`${__dirname}/en/pdp-added.png`);
+const rackspaceLogo2 = fs.createReadStream(`${__dirname}/en/dropdown-new.png`);
 
 mg.messages.create(domain, {
   from: fromEmail,
   to: toEmails,
   subject: 'Hello',
-  html: '<img src="cid:mailgun.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
-  text: 'Testing some Mailgun awesomness!',
+  html: '<img src="cid:en/minicart.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
+  text: 'Testing some Mailgun awesomness!!!',
   //inline: [mailgunLogo],
-  //attachment: [rackspaceLogo]
+  attachment: [rackspaceLogo1,rackspaceLogo2]
 })
   .then((msg) => console.log(msg))
   .catch((err) => console.log(err));
