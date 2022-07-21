5845d5a31ef4db90d14e40cb44763b25bc5cb14c
imbni <mbnism@gmail.com>
2022-07-20 22:37:38 -0400

    express

--- test-express.js
+++ test-express.js
@@ -6,13 +6,15 @@ const fs = require('fs');
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ username: 'api', key: 'ecf3c15e43250274a71c20578faadd75-18e06deb-4292af6c' });
-const domain = 'sandbox65e99c8a211e40dc8462f54c76a5f21c.mailgun.org';
-const fromEmail = 'Excited User <mailgun@sandbox65e99c8a211e40dc8462f54c76a5f21c.mailgun.org>';
+const domain = process.env.MAILGUN_DOMAIN;
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+
+const fromEmail = 'Excited User <mbnism@gmail.com>';
 const toEmails = ['mbnism@gmail.com'];
-const mailgunLogo = fs.createReadStream(`${__dirname}/en/new.png`);
-const rackspaceLogo1 = fs.createReadStream(`${__dirname}/en/left-nav.png`);
-const rackspaceLogo2 = fs.createReadStream(`${__dirname}/en/minicart.png`);
+
+//const mailgunLogo = fs.createReadStream(`${__dirname}/mailgun.png`);
+const rackspaceLogo1 = fs.createReadStream(`${__dirname}/en/pdp-added.png`);
+const rackspaceLogo2 = fs.createReadStream(`${__dirname}/en/dropdown-new.png`);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -24,9 +26,9 @@ express()
       from: fromEmail,
       to: toEmails,
       subject: 'Hello',
-      html: '<img src="cid:mailgun.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
-      text: 'Testing some Mailgun awesomness!',
-      inline: [mailgunLogo],
+      html: '<img src="cid:en/minicart.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
+      text: 'Testing some Mailgun awesomness!!!',
+      //inline: [mailgunLogo],
       attachment: [rackspaceLogo1,rackspaceLogo2]
     })
       .then((msg) => console.log(msg))
