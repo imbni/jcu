8d5b71080fe5845d9dbc2346ab42bf900e2cf615
imbni <mbnism@gmail.com>
2022-07-27 16:21:39 -0400

    test api

--- test-express.js
+++ test-express.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 5000
 
 const fs = require('fs');
@@ -7,7 +11,7 @@ const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
 const domain = process.env.MAILGUN_DOMAIN;
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
 const fromEmail = 'Excited User <mbnism@gmail.com>';
 const toEmails = ['mbnism@gmail.com'];
@@ -17,6 +21,10 @@ const rackspaceLogo1 = fs.createReadStream(`${__dirname}/en/pdp-added.png`);
 const rackspaceLogo2 = fs.createReadStream(`${__dirname}/en/dropdown-new.png`);
 
 express()
+  .use(use(helmet()))
+  .use(use(bodyParser.json()))
+  .use(use(cors()))
+  .use(use(morgan('combined')))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -25,28 +33,35 @@ express()
     mg.messages.create(domain, {
       from: fromEmail,
       to: toEmails,
-      subject: 'Hello',
+      subject: 'Hello :)',
       html: '<img src="cid:en/minicart.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
       text: 'Testing some Mailgun awesomness!!!',
       //inline: [mailgunLogo],
-      attachment: [rackspaceLogo1,rackspaceLogo2]
+      attachment: [rackspaceLogo1, rackspaceLogo2]
     })
       .then((msg) => console.log(msg))
       .catch((err) => console.log(err));
     res.send('done')
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/api', (req, res) => {
+    setTimeout(function () {
+      console.log('This printed after about 1 second');
+      res.send(ads);
+    }, 3000);
 
-  /*
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+/*
 mg.messages.create(domain, {
-  from: fromEmail,
-  to: toEmails,
-  subject: 'Hello',
-  html: '<img src="cid:mailgun.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
-  text: 'Testing some Mailgun awesomness!',
-  inline: [mailgunLogo],
-  attachment: [rackspaceLogo1,rackspaceLogo2]
+from: fromEmail,
+to: toEmails,
+subject: 'Hello',
+html: '<img src="cid:mailgun.png" width="200px"><br><h3>Testing some Mailgun awesomness!</h3>',
+text: 'Testing some Mailgun awesomness!',
+inline: [mailgunLogo],
+attachment: [rackspaceLogo1,rackspaceLogo2]
 })
-  .then((msg) => console.log(msg))
-  .catch((err) => console.log(err));
-  */
+.then((msg) => console.log(msg))
+.catch((err) => console.log(err));
+*/
