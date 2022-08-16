09fa3ff366a1d47953c04e7d85b6698b6d550b9e
imbni <mbnism@gmail.com>
2022-08-16 16:11:23 -0400

    adding signals

--- notification.js
+++ notification.js
@@ -12,7 +12,7 @@ const webhook = new IncomingWebhook(url);
 (async () => {
 		await getEthPrice()
 		.then(data => {
-			console.log(data);
+			//console.log(data);
 			eth = JSON.stringify(data);
 			var objectValue = JSON.parse(eth);
 			console.log(objectValue);
@@ -20,29 +20,29 @@ const webhook = new IncomingWebhook(url);
 		});
 
 
-	// await webhook.send({
+	await webhook.send({
 
-	// 	"type": "home",
-	// 	"blocks": [
-	// 		{
-	// 			"type": "header",
-	// 			"text": {
-	// 				"type": "plain_text",
-	// 				"text": "Buy eth"
-	// 			}
-	// 		},
-	// 		{
-	// 			"type": "section",
-	// 			"text": {
-	// 				"type": "mrkdwn",
-	// 				"text": "*Team Lunch (Internal)*\nCost: *$85.50USD*\nDate: *10/16/2019*\nService Provider: *Honest Sandwiches*  \nExpense no. *<fakelink.toUrl.com|#1797PD>*"
-	// 			}
-	// 		},
-	// 		{
-	// 			"type": "divider"
-	// 		},
-	// 	]
-	// });
+		"type": "home",
+		"blocks": [
+			{
+				"type": "header",
+				"text": {
+					"type": "plain_text",
+					"text": "Buy eth"
+				}
+			},
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": "*Team Lunch (Internal)*\nCost: *$85.50USD*\nDate: *10/16/2019*\nService Provider: *Honest Sandwiches*  \nExpense no. *<fakelink.toUrl.com|#1797PD>*"
+				}
+			},
+			{
+				"type": "divider"
+			},
+		]
+	});
 })();
 
 
