14b23b8bec78d8a5902e57db33346282afd54d07
imbni <mbnism@gmail.com>
2022-08-16 13:42:13 -0400

    adding signals

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const fs = require('fs');
 const { readdirSync, rmSync } = require('fs');
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
+const { IncomingWebhook } = require('@slack/webhook');
+const ethPrice = require('eth-price');
+const url = 'https://hooks.slack.com/services/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';
+const webhook = new IncomingWebhook(url);
 const mailgun = new Mailgun(formData);
 const domain = process.env.MAILGUN_DOMAIN;
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
@@ -20,8 +24,10 @@ const PORT = process.env.PORT || 5000
 //const rackspaceLogo2 = fs.createReadStream(`${__dirname}/en/dropdown-new.png`);
 
 const ads = [
-  { title: 'Hello, world (again)!' }
+  { title: 'done :)' }
 ];
+
+et = '-';
 
 express()
   .use(helmet({
@@ -133,6 +139,57 @@ express()
       readdirSync(dir).forEach(f => rmSync(`${dir}/${f}`));
       res.send('done')
     })();
+  })
+  .get('/buy', (req, res) => {
+
+    (async () => {
+      await getEthPrice()
+        .then(data => {
+          eth = JSON.stringify(data);
+          et = eth;
+        });
+      await webhook.send({
+        "type": "home",
+        "blocks": [
+          {
+            "type": "header",
+            "text": {
+              "type": "plain_text",
+              "text": "Buy eth " + et
+            }
+          }
+        ]
+      });
+      res.send(ads);
+    })();
+
+  })
+  .get('/sell', (req, res) => {
+
+    (async () => {
+      await getEthPrice()
+        .then(data => {
+          eth = JSON.stringify(data);
+          et = eth;
+        });
+      await webhook.send({
+        "type": "home",
+        "blocks": [
+          {
+            "type": "header",
+            "text": {
+              "type": "plain_text",
+              "text": "Sell eth " + et
+            }
+          },
+          {
+            "type": "divider"
+          },
+        ]
+      });
+      res.send(ads);
+    })();
+
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
